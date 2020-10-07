43048a7120bf188bad925eadffbc9956b9506e0c
Ben Singer <bensinger2005@gmail.com>
2020-10-07 16:55:48 -0700

    I just finished the badger program

--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,15 @@
-
+PImage twomad;
 void setup() {
-
+twomad = loadImage("twomad.png");
+size(800, 800);
 }
 void draw() {
+
+twomad.resize(800, 800);
+image(twomad, 0, 0);
+fill(mouseX, mouseY, mouseX-mouseY);
+ellipse(330, 408, 40, 40);
+ellipse(630, 450, 40, 40);
+
 
 }
---
+++ src/_05_for_loops/_2_badgers/badger.java
@@ -0,0 +1,18 @@
+
+package _05_for_loops._2_badgers;
+
+import javax.swing.JOptionPane;
+
+public class badger {
+public static void main(String[] args) {
+	for (int w = 0; w < 2; w++) {
+		for (int i = 0; i < 13; i++) {
+			System.out.print("badger, ");
+			System.out.print(" ");
+		for (int n = 0; n < 2; n++) {
+			System.out.print("mushroom, ");
+		}
+	}
+	}
+}
+}
